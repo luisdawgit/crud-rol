94d80b50f2179336339bc7e0c0e8c59501cdf179
luisdawgit <luisdawgit@gmail.com>
2026-07-06 23:15:49 +0200

    Refinado de PersonajeType

--- src/Form/PersonajeType.php
+++ src/Form/PersonajeType.php
@@ -4,21 +4,49 @@ namespace App\Form;
 
 use App\Entity\Personaje;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Clan;
+
 
 class PersonajeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('experiencia')
-            ->add('nombre')
-            ->add('naturaleza')
-            ->add('conducta')
-            ->add('concepto')
-            ->add('clan')
-        ;
+            ->add('experiencia', NumberType::class, [
+                'label' => 'Puntos de experiencia del personaje',
+                'data' => 0,
+            ])
+
+            ->add(
+                'nombre', TextType::class, [
+                'label' => 'Nombre del personaje'
+                ])
+
+            ->add('clan', EntityType::class, [
+                'class' => Clan::class,
+                'choice_label' => 'nombre',
+                'placeholder' => 'Selecciona un clan',
+                'label' => 'Clan',
+                ])
+
+            ->add('naturaleza', TextType::class,[
+                    'label'=>'Naturaleza del personaje'
+                ])
+
+            ->add('conducta', TextType::class,[
+                    'label'=>'Conducta del personaje'
+                ])
+
+            ->add('concepto', TextType::class,[
+                    'label'=>'Concepto del personaje'
+                ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
